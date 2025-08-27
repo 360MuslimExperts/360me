625ac246aabeb108af20da64581de9ba658b6882
ibtisam-shahid-kh <ibtisam.shahid.kh@gmail.com>
2025-08-27 13:21:18 +0500

    fixation

--- components/Page/PageWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-import Page from "./Page";
-
-export default function PageWrapper() {
-  const { slug } = useParams();
-  return <Page slug={slug} />;
-}
--- pages/page/[slug].js
+++
@@ -1,5 +0,0 @@
-import PageWrapper from "@/components/Page/PageWrapper";
-
-export default function PageSlugPage() {
-  return <PageWrapper />;
-}
