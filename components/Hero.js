c337442c9544cd97544155e19d80b9b4ab5f9c8b
ibtisam-shahid-kh <ibtisam.shahid.kh@gmail.com>
2025-08-30 17:44:24 +0500

    font change and clr

--- components/Hero.js
+++ components/Hero.js
@@ -5,11 +5,11 @@ const Hero = () => {
     <div
       className="w-full min-h-screen bg-[url('/bg-light.jpg')] bg-cover bg-center text-[var(--primary)] flex items-center justify-center"
     >
-      <div className="text-center max-w-3xl px-4">
-        <h1 className="text-5xl md:text-6xl font-extrabold">
+      <div className="text-center max-w-3xl px-4 font-[Outfit]">
+        <h1 className="text-5xl md:text-6xl font-extrabold text-primary">
           Welcome to 360 Muslim Experts
         </h1>
-        <p className="text-lg md:text-2xl font-medium max-w-2xl mx-auto mt-4 mb-6 leading-relaxed">
+        <p className="text-lg font-outfit md:text-2xl font-medium max-w-2xl mx-auto mt-4 mb-6 leading-relaxed">
           Connect with a dynamic network of professionals, scholars, and
           creatives shaping the future. Stay updated on events, insights, and
           opportunities in science, arts, and Islamic research. Let’s grow and
