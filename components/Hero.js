5df7a9edab2b399d3650c0a24a6089ed1b03f83a
ibtisam-shahid-kh <ibtisam.shahid.kh@gmail.com>
2026-07-18 18:51:48 +0500

    update Hero.js replacing bg by svg

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function Hero({ onExplore }) {
@@ -10,26 +9,55 @@ export default function Hero({ onExplore }) {
     <div
       className="relative w-full min-h-screen text-[var(--primary)] flex items-center justify-center overflow-hidden bg-background"
     >
-      {/* Optimized Background Image */}
-      <Image
-        src="/bg-light.jpg"
-        alt="Background"
-        fill
-        priority
-        className="object-cover object-center pointer-events-none"
-        sizes="100vw"
-        quality={75} // Dropped slightly from 85 to 75 for faster mobile delivery
-      />
+      {/* Absolute Background SVG Pattern */}
+      <div className="absolute inset-0 z-0 pointer-events-none w-full h-full opacity-40">
+        <svg 
+          xmlns='http://www.w3.org/2000/svg' 
+          viewBox='0 0 2000 1500'
+          className="w-full h-full object-cover"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <rect fill='#E6B686' width='2000' height='1500'/>
+          <defs>
+            <rect stroke='#E6B686' stroke-width='0.4' width='1' height='1' id='s'/>
+            <pattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <use fill='#e4b485' href='#s' y='2'/><use fill='#e4b485' href='#s' x='1' y='2'/><use fill='#e1b283' href='#s' x='2' y='2'/><use fill='#e1b283' href='#s'/><use fill='#dfb082' href='#s' x='2'/><use fill='#dfb082' href='#s' x='1' y='1'/>
+            </pattern>
+            <pattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#ddaf81'><use href='#s'/><use href='#s' y='5' /><use href='#s' x='1' y='10'/><use href='#s' x='2' y='1'/><use href='#s' x='2' y='4'/><use href='#s' x='3' y='8'/><use href='#s' x='4' y='3'/><use href='#s' x='4' y='7'/><use href='#s' x='5' y='2'/><use href='#s' x='5' y='6'/><use href='#s' x='6' y='9'/></g>
+            </pattern>
+            <pattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#ddaf81'><use href='#s' y='5'/><use href='#s' y='8'/><use href='#s' x='1' y='1'/><use href='#s' x='1' y='9'/><use href='#s' x='1' y='12'/><use href='#s' x='2'/><use href='#s' x='2' y='4'/><use href='#s' x='3' y='2'/><use href='#s' x='3' y='6'/><use href='#s' x='3' y='11'/><use href='#s' x='4' y='3'/><use href='#s' x='4' y='7'/><use href='#s' x='4' y='10'/></g>
+            </pattern>
+            <pattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#daad7f'><use href='#s' y='11'/><use href='#s' x='2' y='9'/><use href='#s' x='5' y='12'/><use href='#s' x='9' y='4'/><use href='#s' x='12' y='1'/><use href='#s' x='16' y='6'/></g>
+            </pattern>
+            <pattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#E6B686'><use href='#s' y='9'/><use href='#s' x='16' y='5'/><use href='#s' x='14' y='2'/><use href='#s' x='11' y='11'/><use href='#s' x='6' y='14'/></g>
+              <g fill='#d8ab7e'><use href='#s' x='3' y='13'/><use href='#s' x='9' y='7'/><use href='#s' x='13' y='10'/><use href='#s' x='15' y='4'/><use href='#s' x='18' y='1'/></g>
+            </pattern>
+            <pattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#4A1D12'><use href='#s' x='2' y='5'/><use href='#s' x='16' y='38'/><use href='#s' x='46' y='42'/><use href='#s' x='29' y='20'/></g>
+            </pattern>
+            <pattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#4A1D12'><use href='#s' x='33' y='13'/><use href='#s' x='27' y='54'/><use href='#s' x='55' y='55'/></g>
+            </pattern>
+            <pattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='rotate(192 1000 750) scale(25.6) translate(-960.94 -720.7)'>
+              <g fill='#4A1D12'><use href='#s' x='11' y='8'/><use href='#s' x='51' y='13'/><use href='#s' x='17' y='73'/><use href='#s' x='99' y='57'/></g>
+            </pattern>
+          </defs>
+          <rect fill='url(#a)' width='100%' height='100%'/><rect fill='url(#b)' width='100%' height='100%'/><rect fill='url(#h)' width='100%' height='100%'/><rect fill='url(#c)' width='100%' height='100%'/><rect fill='url(#d)' width='100%' height='100%'/><rect fill='url(#e)' width='100%' height='100%'/><rect fill='url(#f)' width='100%' height='100%'/><rect fill='url(#g)' width='100%' height='100%'/>
+        </svg>
+      </div>
 
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-b from-background/30 via-background/10 to-background/50 pointer-events-none" />
+      {/* Transparent overlay gradient to ease text contrast */}
+      <div className="absolute inset-0 bg-gradient-to-b from-background/10 via-background/40 to-background/70 pointer-events-none z-0" />
 
-      {/* Added content-visibility to tell the browser this is ready to paint */}
       <div className="relative text-center max-w-4xl px-4 font-[Outfit] z-10 [content-visibility:auto]">
         <motion.h1
-          initial={{ opacity: 0 }} // Removed scale deformation to prevent layout calculation lags
+          initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 0.4, ease: "easeOut" }} // Fast fade-in so LCP triggers immediately
+          transition={{ duration: 0.4, ease: "easeOut" }}
           className="text-5xl md:text-8xl font-black text-primary tracking-tight leading-[1.1]"
         >
           Welcome to <br />
